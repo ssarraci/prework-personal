771902b995cb2246bfa89457f21278954c4ec3d7
Storm <storm.sarracino01@gmail.com>
2021-05-25 15:44:13 -0500

    added index.js, not sure

---
+++ index.js
@@ -0,0 +1,9 @@
+function addingEventListener() {
+    const input = document.getElementById('click';)
+
+    function clickAlert() {
+        alert(`I hope you enjoyed getting introduced to BTS!`)
+    }
+}
+
+input.addEventListener('click', clickAlert);
