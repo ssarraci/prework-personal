cb23ff88a8202a7b4c50461521f0296445781aea
Storm <storm.sarracino01@gmail.com>
2021-05-26 18:46:51 -0500

    Finished the click alert function

---
+++ index.js
@@ -0,0 +1,12 @@
+function addingEventListener() {
+    const input = document.getElementById('input');
+
+function clickAlert() {
+    alert(`I hope you enjoyed the introduction to BTS!`);
+}
+    
+input.addEventListener('click', clickAlert);
+
+}
+
+addingEventListener();
