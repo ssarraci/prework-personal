e02a0aa3751e0be6f31b74907f27a42da8cc1187
Storm <storm.sarracino01@gmail.com>
2021-05-26 16:42:01 -0500

    Got all the photos and member info where I want it

--- index.js
+++
@@ -1,9 +0,0 @@
-function addingEventListener() {
-    const input = document.getElementById('click';)
-
-    function clickAlert() {
-        alert(`I hope you enjoyed getting introduced to BTS!`)
-    }
-}
-
-input.addEventListener('click', clickAlert);
